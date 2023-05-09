9f0b7c70a216e4be39bfdbd46f4975c6ba1aa621
seetha <seethamalakshmimali@gmail.com>
2023-05-09 15:17:02 +0530

    secound

--- prg.py
+++ prg.py
@@ -11,7 +11,8 @@ class FibonacciExample2{
      }    
  }    
  public static void main(String args[]){    
-  int count=10;    
+  int count=10; 
+  maven-2023   
   System.out.print(n1+" "+n2+" "n3+);//printing 0 and 1    
   printFibonacci(count-2);//n-2 because 2 numbers are already printed   
  }  
