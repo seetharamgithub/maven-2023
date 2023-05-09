f169cefcca13520da6e08ea447710a5d94c38fc7
seetharamgithub <113823856+seetharamgithub@users.noreply.github.com>
2023-05-09 15:01:52 +0530

    Update prg.py
    n3 changes

--- prg.py
+++ prg.py
@@ -2,9 +2,10 @@ class FibonacciExample2{
  static int n1=0,n2=1,n3=0;    
  static void printFibonacci(int count){    
     if(count>0){    
-         n3 = n1 + n2;    
+         n4 = n1 + n2 + n3;    
          n1 = n2;    
-         n2 = n3;    
+         n2 = n3; 
+         n3 = n1;
          System.out.print(" "+n3);   
          printFibonacci(count-1);    
      }    
